d7a09bfad92c478b1de4a8287fdbda5e72119b95
Paul Gallimard <paul.gallimard@epitech.eu>
2020-12-28 20:26:41 +0100

    [ADD] cam movement and two rooms

---
+++ Point&Click/Assets/Script/MoveCamLeft.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MoveCamLeft : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public GameObject cam;
+    bool ispressed = false;
+
+    void Update()
+    {
+        if (ispressed)
+        {
+            if (cam.transform.position.x > -16)
+                cam.transform.Translate(-0.2f, 0, 0);
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        ispressed = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        ispressed = false;
+    }
+}
