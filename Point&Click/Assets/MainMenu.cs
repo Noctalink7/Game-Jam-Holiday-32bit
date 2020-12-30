d4d99b27164a4293d577e4b2f22c6b5f306a83ba
guigui <guillaume.jacquin@epitech.eu>
2020-12-30 18:34:06 +0100

    [add]

--- Point&Click/Assets/MainMenu.cs
+++ Point&Click/Assets/MainMenu.cs
@@ -5,18 +5,43 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public string levelToLoad;
+    public GameObject settingsWindow;
+    public GameObject credits;
+    
+
     public void StartGame()
     {
+        SceneManager.LoadScene(levelToLoad);
 
     }
 
     public void SettingsButton()
      {
+         settingsWindow.SetActive(true);
 
      }
+
+      public void CloseSettingsWindow()
+      {
+         settingsWindow.SetActive(false);
+      }  
+
+    public void LanceCredits()
+    {
+        credits.SetActive(true);
+    }
+
+    public void CloseCredits()
+    {
+        credits.SetActive(false);
+
+    }
 
      public void QuitGame()
     {
         Application.Quit();
     }
+
+ 
 }
--- Point&Click/Assets/SettingsMenu.cs
+++ Point&Click/Assets/SettingsMenu.cs
@@ -2,6 +2,8 @@
 using UnityEngine.Audio;
 using UnityEngine.UI;
 using System.Collections.Generic;
+using System.Linq;
+
 public class SettingsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
@@ -11,19 +13,29 @@ public class SettingsMenu : MonoBehaviour
 
     public void Start()
     {
-        resolutions = Screen.resolutions;
+        resolutions = Screen.resolutions.Select(resolution => new Resolution {width = resolution.width, height = resolution.height }).Distinct().ToArray();
         resolutionDropdown.ClearOptions();
         string option = "";
 
         List<string> options = new List<string>();
 
+        int currentResolutionIndex = 0;
+
         for (int i = 0; i < resolutions.Length; i++)
         {
             option = resolutions[i].width + "x" + resolutions[i].height;
             options.Add(option);
+            
+            if (resolutions[i].width == Screen.width && resolutions[i].height == Screen.height) {
+                currentResolutionIndex = i;
+
+            }
         }
 
         resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+        Screen.fullScreen = true;
     }
 
     public void SetVolume(float Volume)
@@ -34,5 +46,11 @@ public class SettingsMenu : MonoBehaviour
     public void SetFullScreen(bool isFullScreen)
     {
         Screen.fullScreen = isFullScreen;
+    }
+
+    public void setResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 }
