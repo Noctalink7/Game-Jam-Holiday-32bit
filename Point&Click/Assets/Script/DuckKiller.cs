6d4684d498f7da7b8143c93c67e0eb50fe3f9202
Paul Gallimard <paul.gallimard@epitech.eu>
2020-12-30 17:15:40 +0100

    [ADD] new decor + score + sound

--- Point&Click/Assets/Script/DuckKiller.cs
+++ Point&Click/Assets/Script/DuckKiller.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DuckKiller : MonoBehaviour
 {
@@ -9,6 +10,9 @@ public class DuckKiller : MonoBehaviour
     public GameObject killerDuck;
     public GameObject bar;
     private int nextUpdate = 10;
+    public Text score;
+    public AudioSource hurt;
+    private float time;
 
     // Start is called before the first frame update
     void Start()
@@ -19,28 +23,39 @@ public class DuckKiller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        time = Time.time;
+     //   Debug.Log(time);
+        score.text = "Time survive : " + Mathf.FloorToInt(Time.time);
         if (Time.time >= nextUpdate)
         {
-            Debug.Log(Time.time + ">=" + nextUpdate);
+//            Debug.Log(Time.time + ">=" + nextUpdate);
             nextUpdate = Mathf.FloorToInt(Time.time) + 10;
+            killerDuck.transform.position = new Vector3(Random.Range(-23, 23), Random.Range(-4, 4), 0);
+            GetComponent<AudioSource>().Play();
             loseHealth();
+        }
+        if (health.currentValue <= 0 || time > 60)
+        {
+            Debug.Log("Game over");
         }
     }
 
     void loseHealth()
     {
-        health.currentValue -= 5;
+        health.currentValue -= 10;
     }
 
     void OnMouseDown()
     {
         life -= 1;
+        hurt.Play();
         if (life == 0)
         {
             bar.transform.localScale = new Vector3(0, 1, 1);
+            time = 0;
             life = 10;
         }
-        nextUpdate = Mathf.FloorToInt(Time.time) + 1;
+        nextUpdate += 1;
         killerDuck.transform.position = new Vector3(Random.Range(-23, 23), Random.Range(-4, 4), 0);
     }
 }
